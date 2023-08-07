e3db86b63fa53e9049cdfd76e401d463d54f0749
onurkaymak <onurkaymak@outlook.com>
2023-08-07 15:35:14 -0700

    combine window alert methods into one call
    Co-authored-by: Jeremy Josol <jeremyjosol@gmail.com>
    Co-authored-by: Seth Gonzales <sethgonzales157@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -19,10 +19,10 @@ function divide(number1, number2) {
 const number1 = parseInt(prompt("Enter a number:"));
 const number2 = parseInt(prompt("Enter another number:"));
 
-window.alert("The sum of your numbers equals: " + add(number1, number2));
+window.alert("The sum of your numbers equals: " + add(number1, number2) + ", the difference of your numbers equals: " + subtract(number1, number2) + ", the product of your numbers equals: " + multiply(number1, number2) + ", and the quotient of your numbers equals: " + divide(number1, number2));
 
-window.alert("The difference of your numbers equals: " + subtract(number1, number2));
+// window.alert("The difference of your numbers equals: " + subtract(number1, number2));
 
-window.alert("The product of your numbers equals: " + multiply(number1, number2));
+// window.alert("The product of your numbers equals: " + multiply(number1, number2));
 
-window.alert("The quotient of your numbers equals: " + divide(number1, number2));
+// window.alert("The quotient of your numbers equals: " + divide(number1, number2));
