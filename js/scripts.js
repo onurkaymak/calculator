0f361da812674aacb1e47995de0cd3ffb3953be6
onurkaymak <onurkaymak@outlook.com>
2023-08-07 15:53:39 -0700

    parse the input the numbers
    Co-authored-by: Jeremy Josol <jeremyjosol@gmail.com>
    Co-authored-by: Seth Gonzales <sethgonzales157@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -15,11 +15,35 @@ function divide(number1, number2) {
     return number1 / number2;
 }
 
-// user interface logic 
-const number1 = parseInt(prompt("Enter a number:"));
-const number2 = parseInt(prompt("Enter another number:"));
+// user interface logic
+const number1 = parseInt(prompt('Enter a number:'));
+const number2 = parseInt(prompt('Enter another number:'));
 
-window.alert("The sum of your numbers equals: " + add(number1, number2) + ", the difference of your numbers equals: " + subtract(number1, number2) + ", the product of your numbers equals: " + multiply(number1, number2) + ", and the quotient of your numbers equals: " + divide(number1, number2));
+window.alert(
+    number1 +
+    ' + ' +
+    number2 +
+    ' = ' +
+    add(number1, number2) +
+    ', ' +
+    number1 +
+    ' - ' +
+    number2 +
+    ' = ' +
+    subtract(number1, number2) +
+    ', ' +
+    number1 +
+    ' * ' +
+    number2 +
+    ' = ' +
+    multiply(number1, number2) +
+    ', ' +
+    number1 +
+    ' / ' +
+    number2 +
+    ' = ' +
+    divide(number1, number2)
+);
 
 // window.alert("The difference of your numbers equals: " + subtract(number1, number2));
 
