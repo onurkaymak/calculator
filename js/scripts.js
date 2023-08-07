ce1095cfa213881fe5bf8b668bdd1cbf7efc6362
onurkaymak <onurkaymak@outlook.com>
2023-08-07 15:26:12 -0700

    add alert for the result of calculations
    Co-authored-by: Jeremy Josol <jeremyjosol@gmail.com>
    Co-authored-by: Seth Gonzales <sethgonzales157@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -19,10 +19,10 @@ function divide(number1, number2) {
 const number1 = parseInt(prompt("Enter a number:"));
 const number2 = parseInt(prompt("Enter another number:"));
 
-window.alert(add(number1, number2));
+window.alert("The sum of your numbers equals: " + add(number1, number2));
 
-window.alert(subtract(number1, number2));
+window.alert("The difference of your numbers equals: " + subtract(number1, number2));
 
-window.alert(multiply(number1, number2));
+window.alert("The product of your numbers equals: " + multiply(number1, number2));
 
-window.alert(divide(number1, number2));
+window.alert("The quotient of your numbers equals: " + divide(number1, number2));
